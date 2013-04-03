a6962e915db0a4a840358097f8f26576377be3af
Ivan Fumagalli <ivan.fumagalli.if@gmail.com>
2013-04-03 18:48:11 +0200

    find_front completa, paraboloide->canale
    Completato find_front.m
    Abbiamo provato a fissare noi una h piccola sul fronte e abbiamo
    cominciato a provare a spegnere la gravità sul fronte: risultati non
    significativi.
    Siamo passati dal problema sul paraboloide a Stoker e Ritter: appena
    cominciati.
    Aggiunto calcolo del volume.
    Ringraziamo Elena per il suo particolare gusto nel sistemare
    indentazione e commenti :)

--- find_front.m
+++ find_front.m
@@ -1,4 +1,4 @@
-function front = find_front(chi_wet)
+function frontnodes = find_front(elements,chi_wet)
 
 % Finds the advancing shoreline nodes, given the characteristic function of
 % wet nodes: a node is said to belong to the shoreline if it is a vertice
@@ -8,6 +8,16 @@ function front = find_front(chi_wet)
 % chi_wet:  chi_wet(i)=1 if i is a wet node
 % front:    vector containing the index of shoreline nodes
 
-
+frontnodes = [];
+for i=1:max(size(elements))
+    if (chi_wet(elements(1,i)) + chi_wet(elements(2,i)) + chi_wet(elements(3,i))>=1) ...
+        & (chi_wet(elements(1,i))+ chi_wet(elements(2,i)) + chi_wet(elements(3,i))<=2)
+        frontnodes = [frontnodes; elements(:,i)];
+%         frontnodes(3*i-2) = elements(1,i);
+%         frontnodes(3*i-1) = elements(2,i);
+%         frontnodes(3*i) = elements(3,i); 
+    end
+    frontnodes = unique(frontnodes);
+end
 
 end
